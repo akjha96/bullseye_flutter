301c4b8dc0a782ffcc1b24d5ef74407726259b03
Anand Jha <akjha96@gmail.com>
2023-02-08 14:10:12 +0530

    landscape mde set as default and only orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const BullsEyeApp();
+  }
+}
+
+class BullsEyeApp extends StatelessWidget {
+  const BullsEyeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return MaterialApp(
         title: 'Bullseye',
         theme: ThemeData(
