215b240681b377ac4f2de5680bd8708679df66e1
Anand Jha <akjha96@gmail.com>
2023-03-19 01:02:47 +0530

    Implemented bonus feature

--- lib/services/current_scrore/current_score.dart
+++ lib/services/current_scrore/current_score.dart
@@ -10,6 +10,14 @@ class CurrentScore {
   int difference(target, currentSliderValue) =>
       differenceInValue = (target - currentSliderValue).abs();
 
-  int calculatedCurrentScore(target, currentSliderValue) =>
-      _maximumScore - difference(target, currentSliderValue);
+  int calculatedCurrentScore(target, currentSliderValue) {
+    int bonus = 0;
+    var difference = this.difference(target, currentSliderValue);
+    if (difference == 0) {
+      bonus = 100;
+    } else if (difference == 1) {
+      bonus = 50;
+    }
+    return _maximumScore - difference + bonus;
+  }
 }
